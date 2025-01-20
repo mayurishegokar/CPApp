453485a6c81bd46e418aa3fa3185ab87a6e0f4f9
mayurishegokar <mayurishegokar152@gmail.com>
2025-01-20 10:43:55 +0530

    one to many relation

--- ProductCategories/src/main/java/com/productcategories/model/Category.java
+++ ProductCategories/src/main/java/com/productcategories/model/Category.java
@@ -1,9 +1,14 @@
 package com.productcategories.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category {
@@ -14,6 +19,9 @@ public class Category {
 	private String categoryName;
 	private String categoryDescription;
 	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Product> product=new ArrayList<>();
+	
 	public Integer getCategoryId() {
 		return categoryId;
 	}
@@ -21,6 +29,13 @@ public class Category {
 		this.categoryId = categoryId;
 	}
 	
+	
+	public List<Product> getProduct() {
+		return product;
+	}
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
 	public String getCategoryName() {
 		return categoryName;
 	}
